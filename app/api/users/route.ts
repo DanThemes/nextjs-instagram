c332abfb792c838d563b5983e34bf24f18adf238
Danthemes <danthemes@gmail.com>
2023-09-06 16:45:36 +0300

    update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,16 @@
+import User from "@/models/User";
+import dbConnect from "@/utils/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   return NextResponse.json({ users: ["1", "2"] });
+}
+
+export async function POST(request: NextRequest) {
+  // const user = await request.json();
+  await dbConnect();
+
+  const user = await User.create({ name: "Dani", email: "dani@test.ro" });
+
+  return NextResponse.json(user, { status: 201 });
 }
