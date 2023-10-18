5e2884897aa48330c7d20c9d72e479f885fa2a58
Danthemes <danthemes@gmail.com>
2023-10-18 16:30:03 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,39 +28,37 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOptions);
+
+  // return (
+  //   <html lang="en">
+  //     <body className={inter.className}>{children}</body>
+  //   </html>
+  // );
+
   return (
     <html lang="en">
       <body className={inter.className}>
-
-         
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-      {children}
-      </body>
-      </html>
-  );
-
-  return (
         <AuthSessionProvider>
           {!session && <TopBar />}
           <div className="flex min-h-full">
             {session && (
               <aside className="bg-white basis-[50px] min-w-[50px] w-[50px] lg:basis:[175px] lg:min-w-[175px] lg:w-[175px] flex-grow-0 border-r border-solid border-r-1 border-[#DBDBDB] relative z-[9]">
                 <div className="fixed h-full flex-[50px] min-w-[50px] w-[50px] lg:flex-[175px] lg:min-w-[175px] lg:w-[175px] no-scrollbar overflow-x-visible max-h-full">
-                  <Sidebar />
+                  {/* <Sidebar /> */}
                 </div>
               </aside>
             )}
-            <main className="px-10 py-10 w-[70rem] mx-auto">{children}</main>
+            <main className="px-10 py-10 w-[70rem] mx-auto">
+              sdsjhd{children}
+            </main>
           </div>
           <div className="z-[999] relative">
-            {/* <AuthModal />
-            <UsersModal />
-            <UploadAvatarModal />
-            <PostModal />
-            <EditPostModal />
-            <EditProfileModal /> */}
+            <AuthModal />
+            {/* <UsersModal /> */}
+            {/* <UploadAvatarModal /> */}
+            {/* <PostModal /> */}
+            {/* <EditPostModal /> */}
+            {/* <EditProfileModal /> */}
           </div>
         </AuthSessionProvider>
       </body>
